25dd9b6bd683d955875d5dd447015f0cbfd5995a
cassidybeni <cassidybeni@pursuit.org>
2021-05-10 21:07:42 -0400

    Options appear in select

--- src/Berries/SelectBerries.js
+++ src/Berries/SelectBerries.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PokemonAPI from "./PokemonAPI"
 
-function SelectBerries() {
-    return (
-        <div>
+export class SelectBerries extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            options: []
+        }
+    }
+
+    async componentDidMount() {
+        const berries = await PokemonAPI.getBerries()
+        this.setState({options: berries})
+    }
+
+    render() {
+        const { options } = this.state
+        return (
+            <div>
             <h1>Select a Type</h1>
-            <select>
-                <option value=""></option>
-            </select>
-        </div>
-    )
+                <select onChange={this.handleChange}>
+                {options.map((option, i) => (
+                    <option key={i}>{option}</option>
+                ))}
+                    <option value=""></option>
+                </select>
+            </div>
+        )
+    }
 }
 
 export default SelectBerries
+
