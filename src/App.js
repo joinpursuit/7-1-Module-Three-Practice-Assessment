6c06efa4199e66b2eaf17e73424ae8c3d0c9a106
PompaDonpa <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-05-11 10:06:40 -0400

    Circle Ci-2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export class App extends Component {
 
         <div className = "home-style">
         <nav className="home-nav">
-        <Link to="/"><img src="https://c0.klipartz.com/pngpicture/506/902/gratis-png-pokemon-omega-ruby-%E2%80%8B%E2%80%8By-alfa-zafiro-pokemon-go-poke-ball-pokemon-go.png" alt="Pokeball" style={{width:"50px", height:"50px"}}/></Link>
+        <Link to="/"><img src="https://c0.klipartz.com/pngpicture/506/902/gratis-png-pokemon-omega-ruby-%E2%80%8B%E2%80%8By-alfa-zafiro-pokemon-go-poke-ball-pokemon-go.png" alt="Pokeball" style={{height:"50px",width:"50px"}}/></Link>
         <Link to="/pokemon">Pokemon</Link>
         <Link to="/locations">Locations</Link>
         <Link to="/berries">Berries</Link>
