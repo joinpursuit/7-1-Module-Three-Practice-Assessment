c06595be694ac705ed88e491a4fe58cf5b6e39c8
PompaDonpa <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2021-05-11 11:26:38 -0400

    Circle Ci-10

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ import './NavBar.css'
 function NavBar() {
     return (
         <nav className="home-nav">
-        <Link to="/"><img src="https://c0.klipartz.com/pngpicture/506/902/gratis-png-pokemon-omega-ruby-%E2%80%8B%E2%80%8By-alfa-zafiro-pokemon-go-poke-ball-pokemon-go.png" alt="Pokeball" height="50px" width="50px" style={{height:"50px", width:"50px"}}/></Link>
+        <Link to="/"><img src="https://www.pngfind.com/pngs/m/158-1587026_pokeball-clipart-pokemon-symbol-free-pokeball-svg-hd.png" alt="Pokeball"/></Link>
         <Link to="/pokemon">Pokemon</Link>
         <Link to="/locations">Locations</Link>
         <Link to="/berries">Berries</Link>
