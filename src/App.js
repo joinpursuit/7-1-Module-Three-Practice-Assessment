ba0d1556e8a4a4b292ddb6c11cb20950e8731f65
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-05-11 16:46:20 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import Berries from "./Components/Berries";
+import Pokemon from "./Components/Pokemon";
+import Locations from "./Components/Locations";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
+      <NavBar />
       <main>
-        <h1>Hello, world!</h1>
+        <Switch>
+          <Route exact path="/">
+            <h1>Welcome to My Pokemon App!</h1>
+          </Route>
+          <Route path="/berries" component={Berries} />
+          <Route path="/pokemon" component={Pokemon} />
+          <Route path="/locations" component={Locations} />
+        </Switch>
       </main>
     </div>
   );
