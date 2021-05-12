0507a8fc242b3ace636b47effe6f94525fadaa00
Talia Pichardo <taliapichardo@gmail.com>
2021-05-12 01:37:32 -0400

    passing all tests

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from 'react-router-dom'
 import NavBar from './Components/NavBar';
 import Berries from './Components/Berries';
 import Pokemon from './Components/Pokemon';
+import Locations from './Components/Locations';
 import "./App.css";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Switch>
         <Route exact path='/'> <h1>Welcome to My Pokemon App!</h1></Route>
         <Route path='/pokemon' component={Pokemon} />
-        {/* <Route path='/locations' component={Locations} />  */}
+        <Route path='/locations' component={Locations} />
         <Route path='/berries' component={Berries} />
       </Switch>
 
--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -4,6 +4,8 @@ import { Component } from 'react';
 class Pokemon extends Component {
     state = {
         searchPokemon: '',
+        pokemon: {},
+        showError: false
     }
 
     handleChange = (e) => {
@@ -21,21 +23,34 @@ class Pokemon extends Component {
             )
             console.log(data)
             this.setState({
-                searchPokemon: ''
+                searchPokemon: '',
+                pokemon: data,
+                showError: false
             })
 
         } catch (error) {
-
+            this.setState({
+                searchPokemon: '',
+                pokemon: {},
+                showError: !this.state.showError
+            })
         }
     }
     render() {
+        const { searchPokemon, pokemon, showError } = this.state
         return (
             <div>
                 <h1>Search for a Pokemon</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} type='text' />
+                    <input onChange={this.handleChange} value={searchPokemon} type='text' placeholder='Find Your Pokemon' />
                     <button type='submit'>Submit</button>
                 </form>
+                {pokemon.name ? (<div>
+                    <h1>Name: {pokemon.name}</h1>
+                    <img src={pokemon.sprites.front_default} alt='pokemon' />
+                    <p>ID {pokemon.id}</p>
+                </div>) : null}
+                {showError ? "Pokemon not found! Try another name or ID number" : null}
             </div>
         )
     }
